3c59fe31ef075178dd8e6d98671d44081578b595
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2024-10-24 10:41:05 +0900

    feat: set IDisposable

--- src/SonaBridge.Core.Mac/MacTalkAutoService.cs
+++ src/SonaBridge.Core.Mac/MacTalkAutoService.cs
@@ -7,6 +7,8 @@ namespace SonaBridge.Core.Mac;
 
 public class MacTalkAutoService : ITalkAutoService
 {
+	private bool _disposedValue;
+
 	public Task<string[]> GetAvailableCastsAsync()
 	{
 		throw new NotSupportedException();
@@ -50,5 +52,34 @@ public class MacTalkAutoService : ITalkAutoService
 	public Task<bool> SpeakAsync(string text, CancellationToken? token = null)
 	{
 		throw new NotSupportedException();
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!_disposedValue)
+		{
+			if (disposing)
+			{
+				// TODO: マネージド状態を破棄します (マネージド オブジェクト)
+			}
+
+			// TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
+			// TODO: 大きなフィールドを null に設定します
+			_disposedValue = true;
+		}
+	}
+
+	// // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+	// ~MacTalkAutoService()
+	// {
+	//     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+	//     Dispose(disposing: false);
+	// }
+
+	public void Dispose()
+	{
+		// このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
 	}
 }
