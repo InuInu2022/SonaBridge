6690971bbeb03bac81070059e1610c0a9c8395df
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2025-11-21 05:11:22 +0900

    feat: Update DestinationExtensions to improve method documentation and refactor extension methods

--- src/SonaBridge.Core.Rest/Extension/DestinationExtensions.cs
+++ src/SonaBridge.Core.Rest/Extension/DestinationExtensions.cs
@@ -7,57 +7,64 @@ using SsBaseInfoDest = SonaBridge.Core.Rest.Internal.Models.Speech_synthesis_bas
 namespace SonaBridge.Core.Rest.Extension;
 
 /// <summary>
-/// <see cref="Destination"/> の拡張メソッドを提供します。
+/// <see cref="Destination"/> 関連の拡張メソッドを提供します。
 /// </summary>
+[SuppressMessage("Naming", "CA1708", Justification = "https://github.com/dotnet/sdk/issues/51716")]
 public static class DestinationExtensions
 {
-	/// <summary>
-	/// <see cref="Destination"/> を <see cref="SsBaseInfoDest"/> に変換します。
-	/// </summary>
-	/// <param name="destination">変換元の  <see cref="Destination"/> 値</param>
-	/// <param name="internalDestination">変換された内部  <see cref="Destination"/> 値</param>
-	/// <returns>変換に成功した場合は <see langword="true"/>、失敗した場合は <see langword="false"/> を返します。</returns>
-	/// <seealso cref="ForceToInternal(Destination)"/>
-	[SuppressMessage("Usage", "SMA0020:Unchecked Cast to Enum Type", Justification = "not casting")]
-	public static bool TryToInternal(
-		this Destination destination,
-		out SsBaseInfoDest? internalDestination
-	)
+	extension(Destination destination)
 	{
-		internalDestination = destination switch
+		/// <summary>
+		/// <see cref="Destination"/> を <see cref="SsBaseInfoDest"/> に変換します。
+		/// </summary>
+		/// <param name="internalDestination">変換された内部  <see cref="Destination"/> 値</param>
+		/// <returns>変換に成功した場合は <see langword="true"/>、失敗した場合は <see langword="false"/> を返します。</returns>
+		/// <seealso cref="ForceToInternal(Destination)"/>
+		[SuppressMessage("Usage", "SMA0020:Unchecked Cast to Enum Type", Justification = "not casting")]
+		public bool TryToInternal(
+			out SsBaseInfoDest? internalDestination
+		)
 		{
-			Destination.AudioDevice => SsBaseInfoDest.Audio_device,
-			Destination.File => SsBaseInfoDest.File,
-			_ => null,
-		};
-		return internalDestination is not null;
+			internalDestination = destination switch
+			{
+				Destination.AudioDevice => SsBaseInfoDest.Audio_device,
+				Destination.File => SsBaseInfoDest.File,
+				_ => null,
+			};
+			return internalDestination is not null;
+		}
+
+		/// <summary>
+		/// <see cref="Destination"/> を <see cref="SsBaseInfoDest"/> に強制変換します。
+		/// 既知の値以外の場合は <see cref="SsBaseInfoDest.Audio_device"/> を返します。
+		/// </summary>
+		/// <returns>変換された内部 <see cref="Destination"/> 値</returns>
+		/// <seealso cref="TryToInternal(Destination)"/>
+		public SsBaseInfoDest ForceToInternal() =>
+			destination switch
+			{
+				Destination.AudioDevice => SsBaseInfoDest.Audio_device,
+				Destination.File => SsBaseInfoDest.File,
+				_ => SsBaseInfoDest.Audio_device,
+			};
 	}
 
 	/// <summary>
-	/// <see cref="Destination"/> を <see cref="SsBaseInfoDest"/> に強制変換します。
-	/// 既知の値以外の場合は <see cref="SsBaseInfoDest.Audio_device"/> を返します。
+	/// <see cref="SsBaseInfoDest"/> の拡張メソッドを提供します。
 	/// </summary>
-	/// <param name="destination">変換元の <see cref="Destination"/> 値</param>
-	/// <returns>変換された内部 <see cref="Destination"/> 値</returns>
-	/// <seealso cref="TryToInternal(Destination)"/>
-	public static SsBaseInfoDest ForceToInternal(this Destination destination) =>
-		destination switch
-		{
-			Destination.AudioDevice => SsBaseInfoDest.Audio_device,
-			Destination.File => SsBaseInfoDest.File,
-			_ => SsBaseInfoDest.Audio_device,
-		};
-
-	/// <summary>
-	/// <see cref="SsBaseInfoDest"/> を <see cref="Destination"/> に変換します。
-	/// </summary>
-	/// <param name="internalDestination">変換元の内部<see cref="Destination"/> 値</param>
-	/// <returns>変換された <see cref="Destination"/> 値</returns>
-	public static Destination ToPublic(this SsBaseInfoDest internalDestination) =>
-		internalDestination switch
-		{
-			SsBaseInfoDest.Audio_device => Destination.AudioDevice,
-			SsBaseInfoDest.File => Destination.File,
-			_ => Destination.Unknown,
-		};
+	/// <seealso cref="DestinationExtensions"/>
+	extension(SsBaseInfoDest internalDestination)
+	{
+		/// <summary>
+		/// <see cref="SsBaseInfoDest"/> を <see cref="Destination"/> に変換します。
+		/// </summary>
+		/// <returns>変換された <see cref="Destination"/> 値</returns>
+		public Destination ToPublic() =>
+			internalDestination switch
+			{
+				SsBaseInfoDest.Audio_device => Destination.AudioDevice,
+				SsBaseInfoDest.File => Destination.File,
+				_ => Destination.Unknown,
+			};
+	}
 }
